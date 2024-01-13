fc4ad69511823e1b26e6da41c8cdf1bf2f0ee7b2
Nitin <67nitinkumar@gmail.com>
2024-01-14 01:39:43 +0530

    Article Summarizer

---
+++ src/services/store.js
@@ -0,0 +1,25 @@
+// import { configureStore } from "@reduxjs/toolkit";
+
+// import { articleApi } from "./article";
+
+// export const store = configureStore({
+//     reducer: {
+//         [articleApi.reducerPath]: articleApi.reducer,
+//     },
+//     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+// })
+
+
+
+////aaadddd
+
+import { configureStore } from "@reduxjs/toolkit";
+
+import { articleApi } from "./article";
+
+export const store = configureStore({
+    reducer: {
+        [articleApi.reducerPath]: articleApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+})
